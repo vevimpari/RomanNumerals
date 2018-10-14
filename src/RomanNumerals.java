2fe1f3c15ab850f3a710ae73716206b201a01aba
veeti <veetivimpari@gmail.com>
2018-10-14 16:14:01 +0300

    part1

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,92 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		int finalNum = 0;
+		int num = 0;
+		int prevNum = 0;
 		
+		if (validateString(romanNum)) {
+			for (int i = 0; i < romanNum.length(); i++) {
+				prevNum = num;
+				num = convertCharacter(romanNum, i);
+				
+				if ((num < prevNum) && (prevNum == 1 || prevNum == 10 || prevNum == 100)) {
+					// subtract
+					finalNum = num-prevNum;
+				} else {
+					finalNum = finalNum + num;
+				}
+			}
+			
+			return finalNum;
+			
+		} else {
+			return 0;
+		}
+	}
+	
+	public int convertCharacter(String romanNum, int i) {
+		char c = romanNum.charAt(i);
+		switch (c) {
+		    case 'M':
+		        return 1000;
+		    case 'D':
+		        return 500;
+		    case 'C':
+		        return 100;
+		    case 'L':
+		        return 50;
+		    case 'X':
+		        return 10;
+		    case 'V':
+		        return 5;
+		    case 'I':
+		        return 1;
+		    default:
+		        return 0;
+		}
+	}
+	
+	public boolean validateString(String romanNum) {
+		boolean isValid = validateCharacters(romanNum);
+		if (isValid) {
+			isValid = validateRepeats(romanNum);
+		}
+		return isValid;
+	}
+	
+	public boolean validateCharacters(String romanNum) {
+	    String pattern = "[M|D|C|L|X|V|I]*";
+	    Pattern p = Pattern.compile(pattern);
+	    Matcher m = p.matcher(romanNum);
+		return m.matches();
+	}
+	
+	public boolean validateRepeats(String romanNum) {
+		boolean isValid = true;
+
+		if (romanNum.contains("IIII") || romanNum.contains("XXXX") || romanNum.contains("CCCC") || romanNum.contains("MMMM")) {
+			isValid = false;
+		}
+		
+		if (romanNum.contains("VV") || romanNum.contains("LL") || romanNum.contains("DD")) {
+			isValid = false;
+		}
+
+		return isValid;
+	}
+
+	
+	public ArrayList<String> convertToList(String romanNum) {
+		ArrayList<String> charList  = new ArrayList<String>();
+		return charList;
+	}
+	
+	public int countI(String romanNum) {
+		int i = romanNum.split("I",-1).length-1;
+		return i;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,123 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	
+	@Test
+	public void romanNumerals_stringIsValid_invalidCharacters() {
+		RomanNumerals rn = new RomanNumerals();		
+		int num = rn.convertToInteger("AA");
+		assertEquals("String contains invalid characaters, return 0", 0, num);
+	}
+	
+	@Test
+	public void romanNumerals_stringIsValid_repeatI() {
+		RomanNumerals rn = new RomanNumerals();		
+		int num = rn.convertToInteger("IIII");
+		assertEquals("String is invalid, I can be repeated at most 3 times, should return 0", 0, num);
+	}
+	
+	@Test
+	public void romanNumerals_stringIsValid_repeatX() {
+		RomanNumerals rn = new RomanNumerals();		
+		int num = rn.convertToInteger("XXXX");
+		assertEquals("String is invalid, X can be repeated at most 3 times, should return 0", 0, num);
+	}
+	
+	@Test
+	public void romanNumerals_stringIsValid_repeatC() {
+		RomanNumerals rn = new RomanNumerals();		
+		int num = rn.convertToInteger("CCCC");
+		assertEquals("String is invalid, C can be repeated at most 3 times, should return 0", 0, num);
+	}
+	
+	@Test
+	public void romanNumerals_stringIsValid_repeatM() {
+		RomanNumerals rn = new RomanNumerals();		
+		int num = rn.convertToInteger("MMMM");
+		assertEquals("String is invalid, M can be repeated at most 3 times, should return 0", 0, num);
+	}
+	
+	@Test
+	public void romanNumerals_testSingleCharacters() {
+		RomanNumerals rn = new RomanNumerals();
+		
+		int num = rn.convertToInteger("I");
+		assertEquals("I should be 1", 1, num);
+		num = rn.convertToInteger("V");
+		assertEquals("V should be 5", 5, num);
+		num = rn.convertToInteger("X");
+		assertEquals("X should be 10", 10, num);
+		num = rn.convertToInteger("L");
+		assertEquals("L should be 50", 50, num);
+		num = rn.convertToInteger("C");
+		assertEquals("C should be 100", 100, num);
+		num = rn.convertToInteger("D");
+		assertEquals("C should be 500", 500, num);
+		num = rn.convertToInteger("M");
+		assertEquals("M should be 1000", 1000, num);
+	}
+	
+	@Test
+	public void romanNumerals_Iequals1() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("I");
+		assertEquals("I should be 1", 1, num);
+	}
+	
+	@Test
+	public void romanNumerals_IIequals2() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("II");
+		assertEquals("II should be 2", 2, num);
+	}
+	
+	@Test
+	public void romanNumerals_IVequals4() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("IV");
+		assertEquals("IV should be 4", 4, num);
+	}
+	
+	@Test
+	public void romanNumerals_VIequals6() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("VI");
+		assertEquals("VI should be 6", 6, num);
+	}
+	
+	@Test
+	public void romanNumerals_VIIequals7() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("VII");
+		assertEquals("VII should be 7", 7, num);
+	}
+	
+	@Test
+	public void romanNumerals_IXequals9() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("IX");
+		assertEquals("IX should be 9", 9, num);
+	}
+	
+	@Test
+	public void romanNumerals_XXequals20() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("XX");
+		assertEquals("XX should be 20", 20, num);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void romanNumerals_LXXequals70() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("LXX");
+		assertEquals("LXX should be 70", 70, num);
+	}
+	
+	@Test
+	public void romanNumerals_LXXXequals80() {
+		RomanNumerals rn = new RomanNumerals();
+		int num = rn.convertToInteger("LXXX");
+		assertEquals("LXXX should be 80", 80, num);
 	}
 
 }
